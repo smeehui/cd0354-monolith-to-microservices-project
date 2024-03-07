ffbba3d11320f6f036ccfb05bc8f48f2bb4ffba4
smeehui <nguyenvanquanghuy99@gmail.com>
2024-03-07 22:54:38 +0700

    init

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=smee
+export POSTGRES_PASSWORD=Unbreakable
+export POSTGRES_HOST=postgres.cc0lus4glgjj.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::my-195124534247-udacit-project3-bucket
 export AWS_REGION=us-east-1
-export AWS_PROFILE=default
+export AWS_PROFILE=udacity
 export JWT_SECRET=testing
-export URL=http://localhost:8100
+export URL=http://localhost:8080
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,9 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0',
+  apiFeedHost: 'http://localhost:8081/api/v0',
+  apiUserHost: 'http://localhost:8080/api/v0',
 };
 
 /*
