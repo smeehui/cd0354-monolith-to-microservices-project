087d117c10e486dca09bd5455be42ce43c149f60
smeehui <nguyenvanquanghuy99@gmail.com>
2024-03-11 20:30:53 +0700

    add ci

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http:// a81f2220654054268986b3b356cd4f20-904121464.us-east-2.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://a81f2220654054268986b3b356cd4f20-904121464.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
